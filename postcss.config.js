0e96598dd72e11a941506109a440f0a4055ebf06
Willem-Jan Boogerd <wjboogerd@users.noreply.github.com>
2024-08-23 09:16:46 +0000

    generated style.json file instead of adding to manifest.json

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   plugins: [
     require('postcss-hash')({
-      manifest: './src/site/_data/manifest.json',
+      manifest: './src/site/_data/style.json',
     }),
     require('autoprefixer'),
     require('cssnano'),
